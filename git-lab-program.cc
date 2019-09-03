baf1034178ed08ddf2d357fa81b12bbc0fa9bbc0
TylerHixon123 <th702717@ohio.edu>
2019-09-03 16:58:19 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Tyler Hixon
+ *        Date: September 3, 2019
+ * Description: This is a piece of code to get familiar with using Github and its commands
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+ 	cout << "Hello Git, it is nice to see you again!!!" << endl;
+    return 0;
+}// main
